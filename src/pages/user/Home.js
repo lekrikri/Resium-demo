759cf76bac1dc2b0905f438a8ba8b0fe33fcc677
lekrikri <ganou.christophe@gmail.com>
2022-12-20 16:58:29 +0100

    Mise a jour

--- src/pages/user/Home.js
+++ src/pages/user/Home.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React from "react";
 import { Background } from "../../GlobalStyles";
 import {
-	MainContainer,
-	Container,
-	Text,
-	HomeBackground,
-	BtnWorkspace,
-	UserContainer
+  MainContainer,
+  Container,
+  Text,
+  HomeBackground,
+  BtnWorkspace,
+  UserContainer,
 } from "../../components/home/Home.styles";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return (
-		<Background>
-			<HomeBackground>
-				<MainContainer />
-				<UserContainer
-					mail="Bryan.bali@gael.fr"
-					fullname="Bryan Bali"
-				/>
-				<Container>
-					<Text>Welcome, massa commodo sem mauris, nunc ridiculus lectus platea. .</Text>
-					<Link 
-					to="/Workspace"
-					style={{textDecoration:"none"}}
-					>
-						<BtnWorkspace />
-					</Link>
-				</Container>
-			</HomeBackground>
-		</Background>
-	);
+  return (
+    <Background>
+      <HomeBackground>
+        <MainContainer />
+        <UserContainer
+          mail="ganou.christophe@gmail.com"
+          fullname="Christophe Ganou"
+        />
+        <Container>
+          <Text>
+            Welcome, massa commodo sem mauris, nunc ridiculus lectus platea. .
+          </Text>
+          <Link to="/Workspace" style={{ textDecoration: "none" }}>
+            <BtnWorkspace />
+          </Link>
+        </Container>
+      </HomeBackground>
+    </Background>
+  );
 };
 
 export default Home;
